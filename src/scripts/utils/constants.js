668f7c5ae9aa3e65bd40b2e8ba3c80a31531ca2e
MenshikovZakhar <94455173+MenshikovZakhar@users.noreply.github.com>
2022-04-03 20:41:46 +0500

    Delete src directory

--- src/scripts/utils/constants.js
+++
@@ -1,9 +0,0 @@
-//объект настроек с селекторами и классами формы;
-export const config = {
-  formSelector: '.popup__forms',
-  inputSelector: '.popup__item',
-  submitButtonSelector: '.popup__submit-button',
-  inactiveButtonClass: 'popup__submit-button_disabled',
-  inputErrorClass: 'popup__item_error',
-  errorClass: 'popup__input-error'
-};
